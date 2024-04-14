185dc0dbfcd6298e55fea3735c2c67c9255cf657
Lukasz Sedek <lukaszsedek@ls-mbp.local>
2024-04-14 20:43:07 +0200

    any

--- titanic-backend/app.py
+++ titanic-backend/app.py
@@ -1,14 +1,6 @@
 from flask import Flask
-from flask_cors import CORS, cross_origin
- 
 app = Flask(__name__)
-CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
- 
- 
-@app.route("/")
-@cross_origin()
-def hello():
-    return {
-        "Status": "True"
-    }
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
